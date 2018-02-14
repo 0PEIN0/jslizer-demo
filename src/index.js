668ee06c6f58cfe21e1381a1b26f3fe66e6341dc
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-02-15 03:05:36 +0600

    Updated demo usage.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import jslizer from 'jslizer';
 
-console.log(343, jslizer);
+console.log(342, jslizer);
+
+class Test extends jslizer.BaseController {
+    constructor() {
+        super();
+        console.log('test constructor called.');
+    }
+}
+var a = new Test();
+console.log(343, a);
